4b891c8fe727ebe0cf4ed4ae3c674c4c455e7a15
Jeremy Chone <jeremy.chone@gmail.com>
2020-09-27 15:14:42 -0700

    + added ico-thumb; ^ upgraded hello-box  click; ^ upgraded var-colors.pcss; ^ upgraded design.sketch file

--- sketchdev.config.js
+++ sketchdev.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	// the origin for the input (design.sketch)
-	origin: 'https://raw.githubusercontent.com/jeremychone/codewalk-s1-sketch-files/main/design-quickstart.sketch',
+	origin: 'https://raw.githubusercontent.com/jeremychone/ketch-files/main/design-quickstart.sketch',
 	// download origin to input if not present (otherwise, needs to do 'npm run sketchdev download' explicitly)
 	download: true,
 
--- src/v-main.ts
+++ src/v-main.ts
@@ -3,17 +3,19 @@ import { BaseHTMLElement, customElement, first, html, onEvent } from 'dom-native
 @customElement('v-main') // same as customElements.define('v-main', IcoElement) 
 class MainView extends BaseHTMLElement { // extends native HTMLElement
 
+	#clickCount = 0; // private, transpiled by TypeScript.
+
 	@onEvent('pointerup', '.hello-box')
 	onHelloClick(evt: PointerEvent) {
-		first(this, '.hello-box strong')!.textContent = 'CLICKED';
+		first(this, '.hello-box strong')!.textContent = `CLICKED ${++this.#clickCount}`;
 	}
 
 	init() { // called once on the first connectedCallback
 
 		this.append(html`
 			<div class="hello-box">
-				<c-ico href="#ico-check"></c-ico>
-				Hello <strong>World!!!</strong>
+				<c-ico href="#ico-thumb"></c-ico>
+				Hello <strong>World</strong>
 			</div>`);
 
 	}
